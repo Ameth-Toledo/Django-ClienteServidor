50fc8b18875a0e4574e71246f782db5b23dc8ff0
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-01-20 18:15:53 -0600

    firts comit

---
+++ tutorial/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+def index(request):
+    return HttpResponse("Hola, mundo!")
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
